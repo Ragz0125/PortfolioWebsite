4c3167499cd0e93de7eea3e13b3c7fa9be5163e2
Raghav <raghav.rajaraman@gmail.com>
2025-01-21 10:28:30 +0530

    ES Lint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
-      "typescript-eslint/no-unused-vars": "off",
-      "typescript-eslint/no-explicit-any": "off",
-      "react/no-unescaped-entities": "off",
+      'typescript-eslint/no-unused-vars': 'off',
+      'typescript-eslint/no-explicit-any': 'off',
+      'react/no-unescaped-entities': 'off',
     },
   }),
 ];
