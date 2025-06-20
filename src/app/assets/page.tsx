97efc9e727a2adfc76e819c38b3f84c7e19028cf
Raghav <raghav.rajaraman@gmail.com>
2025-06-20 11:09:06 +0530

    Fixes

--- src/app/assets/page.tsx
+++
@@ -1,8 +0,0 @@
-export const WaveIcon = () => {
-  return (
-   <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 25.792H25.6V0.191999H0V25.792Z" fill="black"/>
-</svg>
-
-  );
-};
